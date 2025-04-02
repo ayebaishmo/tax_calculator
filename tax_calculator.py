5491928be9eaab3c5c3aabc354281a6aae77c4ce
ayebaishmo <ishmo255super@gmail.com>
2025-04-02 19:00:57 +0300

    Green  test passed

--- tax_calculator.py
+++ tax_calculator.py
@@ -11,4 +11,5 @@ def calculate_tax(earning):
     elif earning <= 36000:
         return (earning - 12000) * 0.2
     
-    
+    else:
+        return (36000 - 12000) * 0.2 + (earning - 36000) * 0.4
