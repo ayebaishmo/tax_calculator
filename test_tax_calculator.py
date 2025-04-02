5166e9e9aa9dfa468de15943f1bdf398e379e92b
ayebaishmo <ishmo255super@gmail.com>
2025-04-02 18:51:24 +0300

    Red test to be failed

--- test_tax_calculator.py
+++ test_tax_calculator.py
@@ -3,6 +3,7 @@ from tax_calculator import calculate_tax
 
 def test_calculate_tax():
     assert calculate_tax(0) == "No tax"
+    assert calculate_tax(5000) == 0
 
 def test_negative_earnings():
     with pytest.raises(ValueError, match="Earnings cannot be negative"):
